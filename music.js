ffb50b5449fb29e53b56a7e4b00c58704538b912
Spencer Hirata <hirataspencemama@gmail.com>
2017-08-23 18:42:39 -0700

    first commit

--- music.js
+++ music.js
@@ -1,6 +1,7 @@
-const musicInfo = [];
+let musicInfo = [];
 
 function addSongFromField(event) {
+  $('#list-result-info').text('')
   event.preventDefault();
 
   const info = $('#musicField').eq(0).val();
@@ -11,6 +12,13 @@ function addSongFromField(event) {
 }
 
 $('#addButton').click(addSongFromField);
+
+$('#clearSearch').click(function(){
+  $('#list-result-info').text('')
+  musicInfo = [];
+  renderList();
+});
+
 $('#musicField').keyup(function(event) {
   if (event.which == 13) { // User presses Enter
     addSongFromField(event);
@@ -24,13 +32,24 @@ function renderList() {
 
   for (const info of musicInfo) {
     const $item = $('<li class="list-group-item">').text(info);
-
     $list.append($item)
   }
 }
 
 $('#getPlaylistBtn').click(function (event) {
-  // TODO: Display a list of music.
-  // You may use anything from musicInfo.
-  console.log('Testing Music Call');
+  $('#list-result-info').text('')
+  let url = "https://itunes.apple.com/search?term=" + musicInfo.join("+") +"&limit=25.json";
+  $.get(url).then(function(data){
+    for(var x = 0; x < 25; x +=1){
+      let resultsA = (JSON.parse(data)).results[x].artistName;
+      let resultsS = (JSON.parse(data)).results[x].trackName;
+      let result = resultsS + " by " + resultsA;
+      const $item = $('<li class="list-result-item">').text(result);
+      $('#list-result-info').append($item)
+    };
+  }).catch(function(){
+    const $item = $('<li class="list-result-item">').text("Can't find a playlist with given parameters.");
+    $('#list-result-info').append($item)
+  });
 });
+
